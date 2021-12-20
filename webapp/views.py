7a299bc9cb6a984767c1cf0ee8d31b4a3556291a
nargiza-jumagulova <impression1108@gmail.com>
2021-12-20 16:13:28 +0600

    изменили функцию index_view

--- webapp/views.py
+++ webapp/views.py
@@ -1,8 +1,35 @@
 from django.shortcuts import render
-
+from webapp.check import Check
 # Create your views here.
 
 
+secret_numbers = Check.generate_numbers(4)
+print(secret_numbers)
+
+
 def index_view(request):
-    return render(request, 'index.html')
+    if request.method == "GET":
+        return render(request, 'index.html')
+    elif request.method == "POST":
+        global secret_numbers
+        context = {}
+        try:
+            user_numbers = list(map(int, request.POST.get('numbers')))
+            print(user_numbers)
+            check = Check(user_numbers, secret_numbers)
+            error = check.validation()
+            if error:
+                context = {'message': error}
+                return render(request, 'index.html', context)
+            else:
+                result = check.get_result()
+                print(result)
+                if result == "Win":
+                    secret_numbers = Check.generate_numbers(4)
+                    print(secret_numbers)
+                context = {'message': result}
+                return render(request, 'index.html', context)
+        except ValueError:
+            context = {'message': "The value should be integers"}
+            return render(request, 'index.html', context)
 
